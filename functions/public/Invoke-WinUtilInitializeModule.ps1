a3f67aa622a583ecd4978f5487b52746019f4b17
Mr.k <mineshtine28546271@gmail.com>
2025-03-08 14:36:33 +0300

    Simple changes to 'Invoke-WinUtilInitializeModule' function

--- functions/public/Invoke-WinUtilInitializeModule.ps1
+++ functions/public/Invoke-WinUtilInitializeModule.ps1
@@ -1,28 +1,30 @@
 function Invoke-WinUtilInitializeModule {
     <#
     .SYNOPSIS
-        Initializes and imports a specified PowerShell module.
+    Initializes and imports a specified PowerShell module.
 
     .PARAMETER module
-        The name of the module to be installed and imported. If the module is not already available, it will be installed for the current user.
+    The name of the module to be installed and imported. If the module is not already available, it will be installed for the current user.
 
+    .EXAMPLE
+    Invoke-WinUtilInitializeModule -module "PSWindowsUpdate"
     #>
 
     param (
         [string]$module
     )
 
-    Invoke-WPFRunspace -ArgumentList $module -DebugPreference $DebugPreference -ScriptBlock {
+    Invoke-WPFRunspace -ArgumentList "$module" -DebugPreference $DebugPreference -ScriptBlock {
         param ($module)
         try {
             if (-not (Get-Module -ListAvailable -Name $module)) {
-                Write-Host "Installing $module module..."
-                Install-Module -Name $module -Force -Scope CurrentUser
+                Write-Host "Installing '$module' module..."
+                Install-Module -Name "$module" -Force -Scope CurrentUser
             }
             Import-Module $module -ErrorAction Stop
-            Write-Host "Imported $module module successfully"
+            Write-Host "Imported '$module' module successfully"
         } catch {
-            Write-Host "Error importing $module module: $_" -ForegroundColor Red
+            Write-Host "Error importing '$module' module: $_" -ForegroundColor Red
         }
     }
 }
